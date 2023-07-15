b1f01523a21c4e0b63299dae8245f4f3e0eba930
picocreator (Eugene Cheah) <eugeneqin@gmail.com>
2023-07-16 02:26:12 +0800

    working dragon_test

--- RWKV-v5x/dragon_test.py
+++ RWKV-v5x/dragon_test.py
@@ -9,14 +9,23 @@ if __name__ == "__main__":
 
     # Check for a model path
     if len(sys.argv) < 2:
-        print("Usage: python SimpleRWKV.py <model_path>")
+        print("Usage: python SimpleRWKV.py <model_path> <device>")
         exit()
 
     # Get model path
     model_path = sys.argv[1]
 
+    # Get device type
+    device = sys.argv[2] if len(sys.argv) > 2 else 'cpu'
+
+    # Check if device contains 'cuda'
+    if device.find('cuda') >= 0:
+        device = 'cuda'
+    else:
+        device = 'cpu'
+
     # Create the model
-    model = SimpleRWKV(model_path)
+    model = SimpleRWKV(model_path, device=device)
 
     # Run the model
     dragon_prompt = "\nIn a shocking finding, scientist discovered a herd of dragons living in a remote, previously unexplored valley, in Tibet. Even more surprising to the researchers was the fact that the dragons spoke perfect Chinese."
